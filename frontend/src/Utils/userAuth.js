18fc0b551a894761367115a5648fad6951ce5446
Jj-Bit27 <22308051220637@cbtis122.edu.mx>
2024-10-19 14:03:41 -0600

    changes

--- frontend/src/Utils/userAuth.js
+++ frontend/src/Utils/userAuth.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:2000" //process.env.API_BACKEND_URL;
+const API_URL = "http://localhost:2000"
 
 axios.defaults.withCredentials = true;
 
